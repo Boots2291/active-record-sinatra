08641e7908c188e1d2802e0c3fb37fe0568f70d9
Boots2291 <buteramv@gmail.com>
2017-08-16 21:11:29 -0600

    Add work

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -2,4 +2,13 @@ class FilmFile < Sinatra::Base
   get '/films' do
     erb :films_index
   end
+
+  get '/directors' do
+    erb :directors_index
+  end
+
+  get '/directors/:id' do
+    @director = Director.find(params[:id])
+    erb :show
+  end
 end
